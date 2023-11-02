9938fa4a6ffe25b533108d796d7d4a32b1613291
marques-j-robinson <marques.j.robinson@gmail.com>
2023-11-02 00:28:36 -0700

    add hover event listener to grid that changes colors

--- app.js
+++ app.js
@@ -4,7 +4,39 @@ for (let rowId = 0; rowId < gridLength; rowId++) {
     const rowEl = document.createElement("div")
     rowEl.classList.add('row')
     for (let cellId = 0; cellId < gridLength; cellId++) {
-        rowEl.innerHTML += '<div class="cell"></div>'
+        const cellEl = document.createElement("div")
+        cellEl.classList.add('cell')
+        cellEl.addEventListener('mouseover', () => {
+            changeColor(cellEl)
+        })
+        rowEl.appendChild(cellEl)
     }
     containerEl.appendChild(rowEl)
+}
+
+const changeColor = el => {
+    let hexNumbers = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+      ];
+    let hexColorCode = "";
+      for (var i = 0; i < 6; i++) {
+        let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+        hexColorCode += hexNumbers[randomIndex];
+      }
+    el.style.backgroundColor = `#${hexColorCode}`
 }
